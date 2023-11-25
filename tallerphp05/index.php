131089f9ca3d61df984de9da2ac5b317ea491089
Hector Segovia <hecgabosego01@gmail.com>
2023-11-25 14:03:20 -0300

    intento 12381723

---
+++ tallerphp05/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arreglos</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+    <h3>Arreglos con PHP</h3>
+    <?php
+        $nombres[] = "JUAN";
+        $nombres[] = "ROSA";
+        $nombres[] = "JULIO";
+        $nombres[] = "ARACELI";
+        $nombres[] = "SOFIA";
+        //LISTAR
+        for ($indice=0; $indice < count($nombres); $indice++) {
+            echo "Nombre: ".$nombres[$indice]."<br>";
+        }
+    ?>
+        <table class="table">
+            <thead>
+                <th>Nombres</th>
+            </thead>
+            <tbody>
+                <?php
+                    for ($i=0; $i < count($nombres); $i++) { 
+                ?>
+                    <tr><td><?=$nombres[$i];?></td></tr>
+                <?php
+                    }
+                ?>
+            </tbody>
+        </table>
+
+        <table class="table">
+            <thead>
+                <th>Formas de pago</th>
+            </thead>
+            <tbody>
+                <?php
+                    $fpagos = array("Efectivo", "Tarjeta", "Transferencia", "Paypal");
+                    for ($i=0; $i < count($fpagos); $i++) { 
+                ?>
+                    <tr><td><?=$fpagos[$i];?></td></tr>
+                <?php
+                    }
+                ?>
+            </tbody>
+        </table>
+    </div>
+</body>
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/jquery.min.js"></script>
+</html>
